9ba7c326cdadb54e0f64cb399cebfad5d79f3ec4
Johnson <johnson1990hitachi@outlook.com>
2021-08-31 17:14:59 +0800

    新增Product Controller & View

---
+++ BS.MVC.Git/Controllers/productController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BS.MVC.Git.Controllers
+{
+    public class productController : Controller
+    {
+        // GET: product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
